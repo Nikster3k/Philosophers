e09cf9e37d4be1ca35a92b049ab9b3fe28fa0681
nsassenb <nsassenb@student.42vienna.com>
2023-11-02 16:05:34 +0100

    WIP Philosophers

--- main.c
+++ main.c
@@ -6,74 +6,12 @@
 /*   By: nsassenb <nsassenb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/01 12:14:32 by nsassenb          #+#    #+#             */
-/*   Updated: 2023/11/01 15:04:09 by nsassenb         ###   ########.fr       */
+/*   Updated: 2023/11/02 15:42:28 by nsassenb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 #include <stdio.h>
-
-void	ft_destroy_philosophers(t_philo *phil, int count)
-{
-	if (phil == NULL)
-		return ;
-	while (count--)
-	{
-		if (phil[count].fork1 != NULL)
-		{
-			pthread_mutex_destroy(phil[count].fork1);
-			free(phil[count].fork1);
-		}
-		if (phil[count].fork2 != NULL)
-		{
-			pthread_mutex_destroy(phil[count].fork1);
-			free(phil[count].fork2);
-		}
-	}
-}
-
-//could stop initializing philosophers when encounter error and
-//meanwhile count. Then use that count to also destroy the philos
-//that way I know that when error 1 occured it was malloc fail.
-//and error 2 I know that the mutex init failed and I dont have
-//to destroy the mutex, which could maybe cause errors when it has
-//not been initialized.
-
-int	ft_init_philosophers(t_philo *philos, int count, t_lifedata data)
-{
-	int		i;
-
-	i = 0;
-	while (i < count)
-	{
-		philos[i].data = data;
-		philos[i].data.nbr = i + 1;
-		philos[i].fork1 = calloc(sizeof(pthread_mutex_t), 1);
-		philos[i].fork2 = calloc(sizeof(pthread_mutex_t), 1);
-		if (philos[i].fork2 == NULL || philos[i].fork1 == NULL)
-			return (1);
-		if (pthread_mutex_init(philos[i].fork2, NULL))
-			return (2);
-		if (i != 0)
-			philos[i].fork1 = philos[i - 1].fork2;
-		i++;
-	}
-	philos[0].fork1 = philos[count - 1].fork2;
-	return (0);
-}
-
-int	ft_init_data(int argc, char **argv, t_lifedata *data)
-{
-	int	count;
-
-	count = ft_atol(argv[1]);
-	data->ttd = ft_atol(argv[2]);
-	data->tte = ft_atol(argv[3]);
-	data->tts = ft_atol(argv[4]);
-	if (argc == 6)
-		data->mineat = ft_atol(argv[5]);
-	return (count);
-}
 
 void	ft_print_data(t_lifedata data)
 {
@@ -84,11 +22,89 @@ void	ft_print_data(t_lifedata data)
 	printf("Min amount of eat: %i\n", data.mineat);
 }
 
-void	ft_print_philo(t_philo *phil)
+void	ft_print_philos(t_philo *phil, int count)
 {
-	ft_print_data(phil->data);
-	printf("thread id: %lu\n", phil->tid);
-	printf("Fork1: %p | Fork2: %p\n", phil->fork1, phil->fork2);
+	int	i;
+
+	i = 0;
+	while (i < count)
+	{
+		ft_print_data(phil[i].data);
+		printf("thread id: %lu\n", phil[i].tid);
+		printf("Fork1: %p | Fork2: %p\n", phil[i].fork1, phil[i].fork2);
+		i++;
+	}
+}
+
+void	ft_eat(t_philo *philo)
+{
+	struct timeval	tv;
+	int				has_eat;
+
+	has_eat = 0;
+	pthread_mutex_lock(philo->fork2);
+	pthread_mutex_lock(philo->fork1);
+	gettimeofday(&tv, NULL);
+	printf("%lu %i is eating\n",
+		(tv.tv_sec * 1000) + (tv.tv_usec / 1000), philo->data.nbr);
+	usleep(philo->data.tte * 1000);
+	has_eat = 1;
+	philo->lifecount = -philo->data.tte;
+	pthread_mutex_unlock(philo->fork1);
+	pthread_mutex_unlock(philo->fork2);
+	if (has_eat != 1)
+		return ;
+	gettimeofday(&tv, NULL);
+	printf("%lu %i is sleeping\n",
+		(tv.tv_sec * 1000) + (tv.tv_usec / 1000), philo->data.nbr);
+	usleep(philo->data.tts * 1000);
+	gettimeofday(&tv, NULL);
+	printf("%lu %i is thinking\n",
+		(tv.tv_sec * 1000) + (tv.tv_usec / 1000), philo->data.nbr);
+}
+
+void	*ft_philo_loop(void	*data)
+{
+	t_philo			*philo;
+	struct timeval	start;
+	struct timeval	end;
+
+	philo = data;
+	pthread_detach(philo->tid);
+	printf("Philo %i - Hello\n", philo->data.nbr);
+	while (philo->lifecount < philo->data.ttd)
+	{
+		gettimeofday(&start, NULL);
+		ft_eat(philo);
+		gettimeofday(&end, NULL);
+		philo->lifecount += (((end.tv_sec - start.tv_sec) * 1000)
+				+ ((end.tv_usec - start.tv_usec) / 1000));
+		printf("Philo%d - Took:%f\n", philo->data.nbr, philo->lifecount);
+	}
+	pthread_exit(NULL);
+}
+
+int	ft_start_philos(t_philo *philos, int count)
+{
+	int	i;
+
+	i = 0;
+	while (i < count)
+	{
+		pthread_create(&philos[i].tid, NULL, ft_philo_loop, &philos[i]);
+		i++;
+	}
+	return (SUCCESS);
+}
+
+int	ft_wait_philos(t_philo *philos, int count)
+{
+	int	i;
+
+	i = 0;
+	while (i < count)
+		pthread_join(philos[i++].tid, NULL);
+	return (SUCCESS);
 }
 
 int	main(int argc, char **argv)
@@ -97,18 +113,17 @@ int	main(int argc, char **argv)
 	t_lifedata	data;
 	int			count;
 
-	if (argc < 5)
+	if (argc < 5 || argc > 6)
 		return (1);
 	count = ft_init_data(argc, argv, &data);
 	printf("Number of philos: %i\n", count);
 	philophs = ft_calloc(sizeof(t_philo), count);
 	if (philophs == NULL)
-		return (0);
+		return (MALLOC_FAIL);
 	if (ft_init_philosophers(philophs, count, data))
-		ft_destroy_philosophers(philophs, count);
-	while (count--)
-	{
-		ft_print_philo(&philophs[count]);
-	}
-	free(philophs);
+		return (ft_destroy_philosophers(MALLOC_FAIL, philophs, count));
+	ft_print_philos(philophs, count);
+	ft_start_philos(philophs, count);
+	ft_wait_philos(philophs, count);
+	return (ft_destroy_philosophers(SUCCESS, philophs, count));
 }
--- philo.h
+++ philo.h
@@ -6,7 +6,7 @@
 /*   By: nsassenb <nsassenb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/01 13:40:01 by nsassenb          #+#    #+#             */
-/*   Updated: 2023/11/01 14:43:47 by nsassenb         ###   ########.fr       */
+/*   Updated: 2023/11/02 15:35:41 by nsassenb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,15 @@
 
 # include <pthread.h>
 # include <stdlib.h>
+# include <sys/time.h>
+# include <unistd.h>
+
+enum e_errors
+{
+	SUCCESS = 0,
+	MALLOC_FAIL,
+	MUTEX_INITFAIL,
+};
 
 typedef struct s_lifedata
 {
@@ -25,11 +34,11 @@ typedef struct s_lifedata
 	int	mineat;
 }	t_lifedata;
 
-
 typedef struct s_philo
 {
 	pthread_t		tid;
 	t_lifedata		data;
+	float			lifecount;
 	pthread_mutex_t	*fork1;
 	pthread_mutex_t	*fork2;
 }	t_philo;
@@ -38,5 +47,10 @@ typedef struct s_philo
 int		ft_isdigit(char c);
 long	ft_atol(const char *nptr);
 void	*ft_calloc(size_t nmemb, size_t size);
+
+//initialize_philosophers.c
+int		ft_destroy_philosophers(int err, t_philo *phil, int count);
+int		ft_init_philosophers(t_philo *philos, int count, t_lifedata data);
+int		ft_init_data(int argc, char **argv, t_lifedata *data);
 
 #endif //!PHILO_H
