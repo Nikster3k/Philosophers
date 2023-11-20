d94cde3af9251469f6905625ff44a18e8d1cd532
nsassenb <nsassenb@student.42vienna.com>
2023-11-20 16:26:21 +0100

    Test print speed

--- philo/print_multi.c
+++ philo/print_multi.c
@@ -6,7 +6,7 @@
 /*   By: nsassenb <nsassenb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/19 23:54:13 by nsassenb          #+#    #+#             */
-/*   Updated: 2023/11/20 00:17:56 by nsassenb         ###   ########.fr       */
+/*   Updated: 2023/11/20 16:25:49 by nsassenb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,16 @@
 void	ft_print_action(char *str, t_philo *philo, long time_stamp)
 {
 	static pthread_mutex_t	mutex = PTHREAD_MUTEX_INITIALIZER;
+	long					start;
+	long					end;
 
+	start = ft_currtime();
 	if (philo->state != RUNNING || ft_sim_running(philo) != RUNNING)
 		return ;
 	pthread_mutex_lock(&mutex);
 	if (philo->state == RUNNING && ft_sim_running(philo) == RUNNING)
 		printf("%ld %i %s\n", time_stamp, philo->nbr, str);
 	pthread_mutex_unlock(&mutex);
+	end = ft_currtime();
+	printf("%i spent time in print %ld\n", philo->nbr, end - start);
 }
